2e8dd86892c2b12925bcc6f8b460ecd2229cfc6e
yinglanma <yinglanma@rescomp-15-311882.stanford.edu>
2016-12-06 15:02:21 -0800

    mood picker
    change mood through picker

--- vesta/vesta WatchKit Extension/MoodInterfaceController.swift
+++ vesta/vesta WatchKit Extension/MoodInterfaceController.swift
@@ -12,44 +12,25 @@ import Foundation
 
 class MoodInterfaceController: WKInterfaceController {
 
-    @IBOutlet var moodSugField: WKInterfaceButton!
+    //@IBOutlet var moodSugField: WKInterfaceButton!
     
     @IBOutlet var emojiField: WKInterfaceButton!
+    @IBOutlet var moodPicker: WKInterfacePicker!
     
-    @IBAction func editMood() {
-        self.presentTextInputController(withSuggestions: [], allowedInputMode: WKTextInputMode.plain, completion:{
-            (results) -> Void in
-            let aResult = results?[0] as? String
-            if(!(aResult == nil)){
-                print(aResult)
-                print("SHORT ASR COMPLETED")
-                
-                self.parseMood(str: aResult!)
-                //DispatchQueue.main.asynchronously() {
-                // Update UI
-                //}
-            }//end if
-        })//end show voice menu
-
-        
-    }
+    var moodList: [(String)] = [
+        ("Happy"),
+        ("In Love")]
     
-    func parseMood(str: String){
-        let strArr = str.characters.split{$0 == " "}.map(String.init)
-        for (idx, elem) in strArr.enumerated() {
-            var elem_lo = elem.lowercased()
-            if elem_lo == "happy"{
-                emojiField.setBackgroundImageNamed("happy.png")
-                moodSugField.setTitle("Happy")
-            }
-            if elem_lo == "love" || elem_lo == "romantic"{
-                emojiField.setBackgroundImageNamed("love.png")
-                moodSugField.setTitle("In Love")
-            }
-            
+    @IBAction func moodSelect(_ value: Int) {
+        if value == 0{
+            emojiField.setBackgroundImageNamed("happy.png")
+        }
+        else{
+            emojiField.setBackgroundImageNamed("love")
         }
 
     }
+    
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
@@ -60,6 +41,13 @@ class MoodInterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        let pickerItems: [WKPickerItem] = moodList.map {
+            let pickerItem = WKPickerItem()
+            pickerItem.title = $0
+            return pickerItem
+        }
+        moodPicker.setItems(pickerItems)
+        moodPicker.focus()
     }
 
     override func didDeactivate() {
